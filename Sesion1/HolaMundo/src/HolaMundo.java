5e66a76f82d239dacc5fb277fb7c50e44480e5f9
sonializMB <sony.mu2002@gmail.com>
2025-04-06 23:42:33 -0600

    Cambios

--- Sesion1/HolaMundo/src/HolaMundo.java
+++ Sesion1/HolaMundo/src/HolaMundo.java
@@ -1,6 +1,6 @@
 public class HolaMundo {
     public static void main(String[] args) {
         //Aqui va el codigo del programa
-        System.out.println("¡Hola, Java!");
+        System.out.println("¡Hola, Mundo, Soy sonia y estoy aprendiendo Java !");
     }
 }
